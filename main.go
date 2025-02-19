9827771fa8548dadc964116083229939b5df6c4a
Flarenzy <>
2025-02-19 11:43:30 +0100

    Bind 8080 on localhost to the new server.

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	serveMux := http.NewServeMux()
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: serveMux,
+	}
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-sigChan
+		os.Exit(0)
+	}()
+	fmt.Println("Listening on", server.Addr)
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer func(server *http.Server, ctx context.Context) {
+		err := server.Shutdown(ctx)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}(server, context.Background())
+}
