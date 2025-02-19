47c1b70a4595b786fd42923eea8c9c688e2e410c
Flarenzy <>
2025-02-19 12:43:35 +0100

    Move / to /app and create health check route.

--- main.go
+++ main.go
@@ -10,11 +10,23 @@ import (
 )
 
 func main() {
-	serveMux := http.NewServeMux()
+	mux := http.NewServeMux()
 	server := &http.Server{
 		Addr:    ":8080",
-		Handler: serveMux,
+		Handler: mux,
 	}
+	fileHandler := http.FileServer(http.Dir("."))
+	mux.Handle("/app/", http.StripPrefix("/app", fileHandler))
+	mux.Handle("/healthz", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		_, err := w.Write([]byte("OK"))
+		if err != nil {
+			fmt.Println("Error writing response")
+			return
+		}
+
+	}))
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
 	go func() {
