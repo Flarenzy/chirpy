e8e96c4d7bfabb60153c50cf8635c53d25b694a3
Flarenzy <>
2025-02-21 09:20:09 +0100

    Token generation and validation, as well as tests for that.

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,7 +1,11 @@
 package auth
 
 import (
+	"errors"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
+	"time"
 )
 
 func HashPassword(password string) (string, error) {
@@ -18,4 +22,35 @@ func CheckPasswordHash(password, hash string) bool {
 		return false
 	}
 	return true
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	signedToken, err := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer:    "chirpy",
+		Subject:   userID.String(),
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiresIn)),
+		IssuedAt:  jwt.NewNumericDate(time.Now()),
+	}).SignedString([]byte(tokenSecret))
+	if err != nil {
+		return "", err
+	}
+	return signedToken, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &jwt.RegisteredClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(tokenSecret), nil
+	})
+	if err != nil {
+		return uuid.Nil, err
+	}
+	tokenClaims, ok := token.Claims.(*jwt.RegisteredClaims)
+	if !ok {
+		return uuid.Nil, errors.New("invalid token")
+	}
+	tokenID, err := uuid.Parse(tokenClaims.Subject)
+	if err != nil {
+		return uuid.Nil, err
+	}
+	return tokenID, nil
 }
---
+++ internal/auth/auth_test.go
@@ -0,0 +1,134 @@
+package auth
+
+import (
+	"github.com/google/uuid"
+	"testing"
+	"time"
+)
+
+func TestCheckPasswordHash(t *testing.T) {
+	data := []string{"password1", "password2", "password3"}
+	for _, password := range data {
+		hashedPW, err := HashPassword(password)
+		if err != nil {
+			t.Errorf("Error hashing password: %v", err)
+			continue
+		}
+		if !CheckPasswordHash(password, hashedPW) {
+			t.Errorf("Passwords don't match, pw: %v, hash: %v", password, hashedPW)
+		}
+	}
+}
+
+func TestMakeJWT(t *testing.T) {
+	t.Parallel()
+	testData := []struct {
+		testName    string
+		id          uuid.UUID
+		tokenSecret string
+		expiration  time.Duration
+	}{
+		{"TestMakeJWT", uuid.New(), "SecretToken", time.Minute},
+	}
+	for _, data := range testData {
+		t.Run(data.testName, func(t *testing.T) {
+			_, err := MakeJWT(data.id, data.tokenSecret, data.expiration)
+			if err != nil {
+				t.Errorf("Error creating JWT: %v", err)
+				t.FailNow()
+			}
+		})
+	}
+}
+
+func TestValidateJWT(t *testing.T) {
+	t.Parallel()
+	testData := []struct {
+		testName    string
+		id          uuid.UUID
+		tokenSecret string
+		expiration  time.Duration
+	}{
+		{"TestValidateJWT", uuid.New(), "SecretToken", time.Hour},
+	}
+	for _, data := range testData {
+		t.Run(data.testName, func(t *testing.T) {
+			jwt, err := MakeJWT(data.id, data.tokenSecret, data.expiration)
+			if err != nil {
+				t.Errorf("Error creating JWT: %v", err)
+				t.FailNow()
+			}
+			validateJWT, err := ValidateJWT(jwt, data.tokenSecret)
+			if err != nil {
+				t.Errorf("Error validating JWT: %v", err)
+				t.FailNow()
+			}
+			if validateJWT != data.id {
+				t.Errorf("ID does not match, validateJWT: %v, validateJWT: %v", data.id, validateJWT)
+			}
+		})
+	}
+}
+
+func TestExpiredToken(t *testing.T) {
+	t.Parallel()
+	testData := []struct {
+		testName    string
+		id          uuid.UUID
+		tokenSecret string
+		expiration  time.Duration
+	}{
+		{"TestExpiredToken", uuid.New(), "SecretToken", time.Second},
+	}
+	for _, data := range testData {
+		t.Run(data.testName, func(t *testing.T) {
+			jwt, err := MakeJWT(data.id, data.tokenSecret, data.expiration)
+			if err != nil {
+				t.Errorf("Error creating JWT: %v", err)
+			}
+			_, err = ValidateJWT(jwt, data.tokenSecret)
+			if err != nil {
+				t.Errorf("Token should be valid before expiring: %v", err)
+			}
+			time.Sleep(data.expiration * 2)
+			_, err = ValidateJWT(jwt, data.tokenSecret)
+			if err != nil {
+				if err.Error() != "token has invalid claims: token is expired" {
+					t.Errorf("Expected that toket has expired: %v", err)
+				}
+			}
+		})
+	}
+}
+
+func TestWrongTokenSecret(t *testing.T) {
+	t.Parallel()
+	testData := []struct {
+		testName    string
+		id          uuid.UUID
+		tokenSecret string
+		expiration  time.Duration
+	}{
+		{"TestWrongTokenSecret", uuid.New(), "SecretToken", time.Hour},
+	}
+	for _, data := range testData {
+		t.Run(data.testName, func(t *testing.T) {
+			jwt, err := MakeJWT(data.id, data.tokenSecret, data.expiration)
+			if err != nil {
+				t.Errorf("Error creating JWT: %v", err)
+			}
+			_, err = ValidateJWT(jwt, "WrongTokenSecret")
+			if err != nil {
+				if err.Error() != "token signature is invalid: signature is invalid" {
+					t.Errorf("Expected that the token secret is wrong %v", err)
+				}
+			}
+		})
+	}
+	_, err := ValidateJWT("not.a.token", "WrongTokenSecret")
+	if err != nil {
+		if err.Error() != "token is malformed: could not JSON decode header: invalid character '\\u009e' looking for beginning of value" {
+			t.Errorf("Expected that the token is malformed %v", err)
+		}
+	}
+}
